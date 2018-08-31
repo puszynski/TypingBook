b6caf73c8d1dd4bea78a6e506bfdac3facbafb4b
puszynski <33966744+puszynski@users.noreply.github.com>
2018-09-01 01:09:27 +0200

    Add xUnit and some test-test

---
+++ UnitTests/BookDomainModelTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class BookDomainModelTest //wskazuje jaki kontroller(klasa) jest testowany(a) ?
+    {
+        [TestMethod]
+        public void VerifyBook()
+        {
+            // arrange
+            const int expectedID = 1;
+            const string expectedTitle = "TestName";
+            const string expectedAuthor = "TestAuthor";
+            const string expectedContent = "TestContent";
+
+            
+            FakeBookRepository repositoryParam = new FakeBookRepository();
+            repositoryParam.Book.Add(test01);
+            // act
+            var book = new Book()
+            {
+                ID = expectedID,
+                Title = expectedTitle,
+                Author = expectedAuthor,
+                Content = expectedContent
+            };
+
+            // assert
+            Assert.AreEqual();
+        }
+    }
+
+    private class FakeBookRepository : IBookRepository
+    {
+        public List<Book> Books = new List<Book>();
+        public bool DidSubmitChanges = false; 
+
+        public void AddBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
