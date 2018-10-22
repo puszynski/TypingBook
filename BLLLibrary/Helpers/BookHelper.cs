e1512aca50cacf96cf97093994d07a255518a7cf
puszynski <33966744+puszynski@users.noreply.github.com>
2018-10-22 07:22:59 +0200

    DivideMethod vol 2

--- BLLLibrary/Helpers/BookHelper.cs
+++ BLLLibrary/Helpers/BookHelper.cs
@@ -12,67 +12,72 @@ namespace BLLLibrary.Helpers
         { 
             const int minAndMaxLenghtOfBothParts = 200;
             var rest = bookContent; // Use string builder?      
-            rest = "Hi! :) TypingBook is simple but smarth app that will rise yours quick and accuracy typing on keyboard and english words knowleadge - rewritng is Key here. Those skills will help you on many areas. But in the same time, you can relax by typing your favorite books or use one of recomended.Skill up, learn and take a fun! Switch to Light Layout if you prefer. Use minimise option to size TypingWindow to minimum, so you can e.g. watch movie in the same time. By default we will use cookies to save your actuall progress on your computer. You have access to our library of public books. If you need more - add your own books or load your notes to rewrite it(its briliant excersize) or get more advance statistisc - please LogIN so we can store your data in our Data Base. Now please enjoy the journey with default book - IN TO THE WILD by Jon Krakauer.";
+            rest = "Hi! :) TypingBook is simple but smarth app that will rise yours quick and accuracy typing on keyboard and english words knowleadge - rewritng is Key here. Those skills will help you on many areas. But in the same time, you can relax by typing your favorite books or use one of recomended. Skill up, learn and take a fun! Switch to Light Layout if you prefer. Use minimise option to size TypingWindow to minimum, so you can e.g. watch movie in the same time. By default we will use cookies to save your actuall progress on your computer. You have access to our library of public books. If you need more - add your own books or load your notes to rewrite it(its briliant excersize) or get more advance statistisc - please LogIN so we can store your data in our Data Base. Now please enjoy the journey with default book - IN TO THE WILD by Jon Krakauer.";
             var firstPart = "";
             var secondPart = "";
-            var bookPage = new List<string>();
+            var bookPages = new List<string>();
+            var EndDone = false;
 
             while (rest != "")
             {
-                // First part
-                if (IsEnd(rest, true))
+                if (IsEnd(rest))
                     firstPart = rest.Substring(0, minAndMaxLenghtOfBothParts);
-                if (IsEnd(rest))                    
-                    rest = rest.Substring(minAndMaxLenghtOfBothParts);                    
+                
+                rest = rest.Replace(firstPart, "");                  
 
-                // Second part
-                if (IsEnd(rest, true, true))
+                if (IsEnd(rest, true))
                 {
                     secondPart = rest.Substring(0, rest.IndexOf(". ") + 1);
 
                     if (secondPart.Length >= minAndMaxLenghtOfBothParts)
                         secondPart = secondPartAlternatives(rest);
                 }
-                if (secondPart.Length == rest.Length)
-                    rest = "";
-                else
-                    rest = rest.Substring(rest.IndexOf(". ") + 2);
-               
-                // Whole part
-                if (IsEnd(rest) || firstPart != "")
-                {
-                    bookPage.Add(firstPart + secondPart);
-                    firstPart = "";
-                    secondPart = "";
-                }
-                    
+                
+                rest = rest.Replace(secondPart, "");
+                rest = removeStartSpaces(rest);               
+                
+                bookPages.Add(firstPart + secondPart);
+                firstPart = "";
+                secondPart = "";                                    
             }
-            return bookPage;
+            return bookPages;
 
 
-            bool IsEnd(string _rest, bool canBeEnd = false, bool IsSecondPart = false)
+           
+            bool IsEnd(string input, bool IsSecondPart = false)
             {
-                var result = _rest.Length >= minAndMaxLenghtOfBothParts ? true : false;
-                if (result == false && canBeEnd)
+                var result = input.Length >= minAndMaxLenghtOfBothParts ? true : false;
+
+                if (result == false && EndDone == false)
                 {
+                    EndDone = true;
+
                     if (IsSecondPart)
-                    {
-                        secondPart = rest;
-                    }                        
+                        secondPart = rest;  
                     else
                         firstPart = rest;
+
                     rest = "";
                 }
                 return result;
             }
 
 
-            string secondPartAlternatives(string _rest)
+            string secondPartAlternatives(string input)
             {
-                var  _secondPart = _rest.Substring(0, rest.IndexOf(", ") + 1);
-                if (_secondPart.Length >= minAndMaxLenghtOfBothParts)
-                    _secondPart = _rest.Substring(0, rest.IndexOf(" ") + 1);
-                return _secondPart;
+                var  result = input.Substring(0, rest.IndexOf(", ") + 1);
+                if (result.Length >= minAndMaxLenghtOfBothParts)
+                    result = input.Substring(0, rest.IndexOf(" ") + 1);
+                return result;
+            }
+
+            string removeStartSpaces(string input)
+            {
+                var result = input;
+                while (result.Substring(0, 1) == " ")
+                    result = input.Substring(1, result.Length-1);
+
+                return result;
             }
         }
         #endregion
