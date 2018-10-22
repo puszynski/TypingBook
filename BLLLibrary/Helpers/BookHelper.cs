e339eb987358566783b6314ec87acc4d32209e05
puszynski <33966744+puszynski@users.noreply.github.com>
2018-10-22 18:51:39 +0200

    DivideBook small refactor

--- BLLLibrary/Helpers/BookHelper.cs
+++ BLLLibrary/Helpers/BookHelper.cs
@@ -12,20 +12,21 @@ namespace BLLLibrary.Helpers
         { 
             const int minAndMaxLenghtOfBothParts = 200;
             var rest = bookContent; // Use string builder?      
-            rest = "Hi! :) TypingBook is simple but smarth app that will rise yours quick and accuracy typing on keyboard and english words knowleadge - rewritng is Key here. Those skills will help you on many areas. But in the same time, you can relax by typing your favorite books or use one of recomended. Skill up, learn and take a fun! Switch to Light Layout if you prefer. Use minimise option to size TypingWindow to minimum, so you can e.g. watch movie in the same time. By default we will use cookies to save your actuall progress on your computer. You have access to our library of public books. If you need more - add your own books or load your notes to rewrite it(its briliant excersize) or get more advance statistisc - please LogIN so we can store your data in our Data Base. Now please enjoy the journey with default book - IN TO THE WILD by Jon Krakauer.";
+            //rest = "Hi! :) TypingBook is simple but smarth app that will rise yours quick and accuracy typing on keyboard and english words knowleadge - rewritng is Key here. Those skills will help you on many areas. But in the same time, you can relax by typing your favorite books or use one of recomended. Skill up, learn and take a fun! Switch to Light Layout if you prefer. Use minimise option to size TypingWindow to minimum, so you can e.g. watch movie in the same time. By default we will use cookies to save your actuall progress on your computer. You have access to our library of public books. If you need more - add your own books or load your notes to rewrite it(its briliant excersize) or get more advance statistisc - please LogIN so we can store your data in our Data Base. Now please enjoy the journey with default book - IN TO THE WILD by Jon Krakauer.";
             var firstPart = "";
             var secondPart = "";
             var bookPages = new List<string>();
-            var EndDone = false;
+            var IsEnd = false;
 
             while (rest != "")
             {
-                if (IsEnd(rest))
+                if (IsLastPart(rest))
                     firstPart = rest.Substring(0, minAndMaxLenghtOfBothParts);
                 
-                rest = rest.Replace(firstPart, "");                  
+                if(!IsEnd)
+                    rest = rest.Replace(firstPart, "");                  
 
-                if (IsEnd(rest, true))
+                if (IsLastPart(rest, true))
                 {
                     secondPart = rest.Substring(0, rest.IndexOf(". ") + 1);
 
@@ -33,9 +34,12 @@ namespace BLLLibrary.Helpers
                         secondPart = secondPartAlternatives(rest);
                 }
                 
-                rest = rest.Replace(secondPart, "");
-                rest = removeStartSpaces(rest);               
-                
+                if(!IsEnd)
+                {
+                    rest = rest.Replace(secondPart, "");
+                    rest = removeStartSpaces(rest);
+                }
+
                 bookPages.Add(firstPart + secondPart);
                 firstPart = "";
                 secondPart = "";                                    
@@ -44,13 +48,13 @@ namespace BLLLibrary.Helpers
 
 
            
-            bool IsEnd(string input, bool IsSecondPart = false)
+            bool IsLastPart(string input, bool IsSecondPart = false)
             {
                 var result = input.Length >= minAndMaxLenghtOfBothParts ? true : false;
 
-                if (result == false && EndDone == false)
+                if (result == false && IsEnd == false)
                 {
-                    EndDone = true;
+                    IsEnd = true;
 
                     if (IsSecondPart)
                         secondPart = rest;  
