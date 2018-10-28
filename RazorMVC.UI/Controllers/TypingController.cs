2c23b9ae9e4ecae39f6afe8bf49d55f206703d32
puszynski <33966744+puszynski@users.noreply.github.com>
2018-10-28 18:29:38 +0100

    Add typing controller

--- RazorMVC.UI/Controllers/TypingController.cs
+++ RazorMVC.UI/Controllers/TypingController.cs
@@ -8,15 +8,15 @@ using UILibrary.Helpers;
 
 namespace RazorMVC.UI.Controllers
 {
-    public class TypeController : Controller
+    public class TypingController : Controller
     {
         private readonly IDeserializeJsonHelper _deserializeJsonHelper; // IoC: field + ctor + Startup.cs
 
-        public TypeController(IDeserializeJsonHelper deserializeJsonHelper) => _deserializeJsonHelper = deserializeJsonHelper;
+        public TypingController(IDeserializeJsonHelper deserializeJsonHelper) => _deserializeJsonHelper = deserializeJsonHelper;
 
-        public IActionResult Index()
+        public IActionResult Index(int bookID = 1, int bookPage = 1)
         {
-            var apiUrl = "http://localhost:5000/api/book/get/" + Book.ToString() + "/" + bookPage.ToString();
+            var apiUrl = "http://localhost:5000/api/book/get/" + bookID.ToString() + "/" + bookPage.ToString();
             var modelFromAPI = _deserializeJsonHelper.DeserializeJsonData<Book>(apiUrl);
 
             return View(modelFromAPI);
---
+++ RazorMVC.UI/Models/AnchorListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorMVC.UI.Models
+{
+    public class AnchorListViewModel
+    {
+        public string TextToDisplay { get; set; }
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+    }
+}
